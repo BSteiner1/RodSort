89c6c9e812f7b1ded653443ab94df7ad3b541d66
BSteiner1 <96544001+BSteiner1@users.noreply.github.com>
2022-07-31 21:45:43 +0100

    Update

--- Workspace.py
+++ Workspace.py
@@ -1,4 +1,3 @@
-
 def insert_number(list, x):
   
     index = len(list)
@@ -13,22 +12,13 @@ def insert_number(list, x):
       list = list[:index] + [x]
     else:
       list = list[:index] + [x] + list[index:]
-
-    return list, index
-  
-# Driver function
-#list = []
-#list = insert_number(list,3)
-#list = insert_number(list[0],4)
-
-#print(list)
+      
+    return list, index 
 
 from random import randint
 
 
 def insert_by_index(list, num, index, bin):
-    
-    bin = []
     
     if list == []:
         list.append(num)
@@ -37,45 +27,34 @@ def insert_by_index(list, num, index, bin):
         
     if index >= len(list):
         index = len(list)
-        if num >= list[index-1]:
+        if num >= list[-1]:
             list.append(num)
         else:
             bin.append(num)
+            
+        return list, bin
         
     else:
         if num >= list[index-1] and num <= list[index]:
-            list = list[:index] + [num] + list[index:]          #stable sort
+            list.insert(index, num)
         else:
-            print('to bin: ', num)
             bin.append(num)
   
     return list, bin
 
-# =============================================================================
-# list = []
-# list2 = [2,3,1,5,2,7,3,7,9]
-# 
-# recycle_bin = []
-# 
-# for x in list2:
-#     z = randint(0,10)
-#     print(x, 'at', z)
-#     insert_by_index(list, x, z, recycle_bin)[0]
-#     #recycle_bin = recycle_bin + insert_by_index(list, x, 3, recycle_bin)[1]
-#     print(list, ' +', bin)
-#     
-# print(list)
-#     
-# =============================================================================
-
+    
 binn = []
-list = [-3,-2,-1]
-list2 = [1,6,2,6,9]
+list = []
+list2 = [randint(0,10) for i in range(10)]
+
+print(list2)
 
 for x in list2:
-    insert_by_index(list, x, 3, binn)
-    list = insert_by_index(list, x, 5, binn)[0]
+    z = randint(0,10)
+    insert_by_index(list, x, z, binn)
     print(list, binn)
-        
     
-insert_by_index(list, x, 5, binn)[0]
+print('SUM: ', len(list) + len(binn))
+
+
+    
